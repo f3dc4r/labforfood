b3021c044b6e48b039ab063b831624ea92477aaa
f3dc4r <fedcar@gmail.com>
2021-04-06 23:21:04 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'labforfood';
+  title = 'labforfood';  
 }
